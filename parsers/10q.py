f454b5b020bc1b83c6b37b0ac9d9f4dbf8b6009d
Jack Zemke <95448108+jackzemke@users.noreply.github.com>
2024-09-22 18:25:07 -0500

    almost done, double line issues

--- parsers/10q.py
+++ parsers/10q.py
@@ -11,13 +11,26 @@ def is_number_or_parenthesis_number(s, i):
         return True
     return False
 
+def is_hyphen(s, i):
+    # Ensure the index is within valid range
+    if i > 1 and i < len(s) - 2:
+        # print(s[i])
+        return (
+            (s[i - 2].isdigit() and s[i - 1] == ' ' and s[i] == '—') or 
+            (s[i] == '—' and s[i + 1] == ' ' and (s[i + 2] == '—' or s[i + 2].isdigit()))
+        )
+    return False
+
 def hardstringtoint(text):
     text = text.replace(",",'')
     if "(" in text:
         text = text.replace("(",'-')
         text = text.replace(')','')
     text = text.replace("$",'')
-    return(int(text))
+    return(int(float(text)))
+
+def extractheaders(text):
+    text = text.replace(',',)
 
 
 
@@ -29,21 +42,24 @@ def netincomevar(read):
     for i in range(len(read.pages)): #find page number containing net income variances
         text = read.pages[i].extract_text()
         # print(i)
-        if "ENTERGY CORPORATION AND SUBSIDIARIES\nMANAGEMENT" in text and "S FINANCIAL DISCUSSION AND ANALYSIS" in text:
-            # print(f'found it on page {i}')
+        if "Following are income statement variances for" in text:
+            print(f'found it on page {i}')
             num = i
             break
     
     page = read.pages[num]
     text = page.extract_text()
+    # print(text)
     
     out = {}
     out['Net Income Variance'] = {}
     key = ''
 
-    for i in text:
-        key += i
-        if i == '\n':
+    for i in range(len(text)):
+        key += text[i]
+        # print(key)
+        if text[i] == '\n':
+            # print(key)
 
             if "Quarter" in key:
                 quarter = ''
@@ -51,27 +67,62 @@ def netincomevar(read):
                     quarter += j
                     if j == 'C':
                         quarter = quarter[:len(quarter)-2]
-                        # print(quarter)
+                        print(quarter)
                         out['Quarter'] = quarter
+
+            if "Following are income statement variances for" in key:
+
+                keyadd = ''
+                if "comparing" not in key:
+                    # print("*****" + i)
+                    for k in range(1,30):
+                        keyadd += text[i+k]
+                        if "comparing" in keyadd:
+                            keyadd = keyadd.split()
+                            break
+                    
+                
+                temp = key.split()
+                temp.extend(keyadd)
+
+                print()
+                print("TESTING IDENTIFICATIONS")
+                # print(temp)
+                head = []
+                header = []
+                for j in range(temp.index("for")+1,temp.index('comparing')):
+                    # print(temp[j])
+                    head.append(temp[j])
+                    if ',' in temp[j]:
+                        l = ' '.join(head)
+                        l = l.replace(',','')
+                        header.append(l)
+                        l = ''
+                        head = []
+                header.append(temp[temp.index('comparing')-1])
+                print(header)
+                # print(key)
+                print()
 
             if bool(re.search(r'\d+,\d+', key)):
                 name = ''
                 index = 0
-                # print(key)
+                # print(f'key: {key}')
                 for j in key:
-                    if is_number_or_parenthesis_number(key,index) and "Net Income" not in key:
-                        # print(name)
+                    if (is_number_or_parenthesis_number(key,index) or is_hyphen(key,index)) and "Net Income" not in key:
                         key = key.replace(name,'')
                         key = key.replace('—','0')
                         # print(key)
                         key = key.split()
+                        name = name.replace('$','')
                         
-                        # print(key)
+                        print(f'name: {name}')
+                        print(f'ending key: {key}')
+                        
                         out['Net Income Variance']['Scale'] = 'Thousands of Dollars'
                         out['Net Income Variance'][name] = {}
-                        out['Net Income Variance'][name]['Utility'] = hardstringtoint(key[0])
-                        out['Net Income Variance'][name]['Parent & Other'] = hardstringtoint(key[1])
-                        out['Net Income Variance'][name]['Entergy'] = hardstringtoint(key[2])
+                        for i in range(len(key)):
+                            out['Net Income Variance'][name][header[i]] = hardstringtoint(key[i])
                         break
 
                     name += j
@@ -79,7 +130,7 @@ def netincomevar(read):
 
             key = ''
 
-    # print(out)
+    print(out)
     return out
 
 
@@ -101,7 +152,7 @@ def tojson(path): #"main" function, writes the results from helper function to a
 
 start = time.time()
 # tojson('etr-20240331.pdf')
-# tojson('etr-20240630.pdf')
+# tojson('../data/10QsEntergy/etr-20230630.pdf')
 print(f'Task completed in {time.time()-start} seconds')
 
 
@@ -113,10 +164,13 @@ def iterate_directory(directory_path):
     # Iterate over all the files in the directory
     for root, dirs, files in os.walk(directory_path):
         for file in files:
+            print()
+            print("*"*10 + "new doc" + "*"*10)
             # Construct the full path of the file
             file_path = os.path.join(root, file)
             # file_path = directory_path
-            if ".json" in file_path:
+            if ".json" in file_path or ".pdf" not in file_path:
+                print(f"doc is {file_path}, skipping")
                 continue
             # Pass the file name to the function
             tojson(file_path)
